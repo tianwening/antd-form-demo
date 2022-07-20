3dd1a21b73e93d6dfd71c3eff0f6e7899de0fe20
changfei02 <changfei02@meituan.com>
2022-07-20 18:54:50 +0800

    feat: 修改publicPath

--- config-overrides.js
+++ config-overrides.js
@@ -3,5 +3,6 @@ const { resolve } = require("path")
 
 module.exports = function override(config, env) {
   config.output.path = resolve(__dirname, 'docs')
+  config.output.publicPath = 'https://tianwening.github.io'
   return config;
 }
